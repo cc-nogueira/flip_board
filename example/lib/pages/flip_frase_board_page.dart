b504be8ffec4191deb6e813ae4fe153e4112290c
ricardo-nogueira-cc <ricardo@nogueira.cc>
2021-07-21 16:58:55 -0300

    FlipFraseBoard example page

--- example/lib/pages/flip_frase_board_page.dart
+++ example/lib/pages/flip_frase_board_page.dart
@@ -7,7 +7,20 @@ import 'package:flutter/material.dart';
 /// and from "AAAAAAAAAA" to "FLIP BOARD" on the second line
 /// FlipFraseBoard uses Theme colors and optional parameterized colors.
 /// Chars are flipped in different random speeds.
-class FlipFraseBoardPage extends StatelessWidget {
+class FlipFraseBoardPage extends StatefulWidget {
+  @override
+  _FlipFraseBoardPageState createState() => _FlipFraseBoardPageState();
+}
+
+class _FlipFraseBoardPageState extends State<FlipFraseBoardPage> {
+  final _completed = [false, false];
+  final _startNotifier = ValueNotifier(0);
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(title: const Text('Flip Frase Board')),
@@ -24,14 +37,31 @@ class FlipFraseBoardPage extends StatelessWidget {
                   endFrase: 'FLUTTER',
                   fontSize: 42.0,
                   letterSpacing: 3.0,
+                  onDone: () => _onDone(0),
+                  flipSpacing: 0.0,
+                  startNotifier: _startNotifier,
                 ),
                 const SizedBox(height: 10.0),
                 FlipFraseBoard(
                   startLetter: 'A',
                   endFrase: 'FLIP BOARD',
                   fontSize: 30.0,
-                  flipSpacing: 0.5,
+                  flipSpacing: 0.8,
                   endColors: _endColors,
+                  onDone: () => _onDone(1),
+                  startNotifier: _startNotifier,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 24.0),
+                  child: (_completed[0] && _completed[1])
+                      ? IconButton(
+                          onPressed: _restart,
+                          icon: const Icon(
+                            Icons.replay_circle_filled,
+                            size: 48.0,
+                          ),
+                        )
+                      : const SizedBox(height: 48.0),
                 ),
               ],
             ),
@@ -48,4 +78,19 @@ class FlipFraseBoardPage extends StatelessWidget {
         Colors.orange[900]!,
         Colors.cyan[800]!,
       ];
+
+  void _onDone(int index) {
+    _completed[index] = true;
+    if (_completed[0] && _completed[1]) {
+      setState(() {});
+    }
+  }
+
+  void _restart() {
+    setState(() {
+      _completed[0] = false;
+      _completed[1] = false;
+      _startNotifier.value = _startNotifier.value + 1;
+    });
+  }
 }
