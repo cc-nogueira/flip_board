7477c5de7c88586e2dc6701d6183f405b7c31332
ricardo-nogueira-cc <ricardo@nogueira.cc>
2021-07-26 17:56:33 -0300

    Improved layout in example/main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,26 +11,21 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flip Board',
-      routes: {
-        'flip_widget': _flipWidgetPage,
-        'flip_image': _flipImagePage,
-        'flip_stream': _flipStreamPage,
-        'flip_frase_board': (_) => FlipFraseBoardPage(),
-        'flip_clock': (_) => FlipClockPage(),
-        'countdown_clock': (_) => CountdownClockPage(),
-      },
-      home: HomePage(),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flip Board',
+        routes: {
+          'flip_widget': (_) => FlipWidgetPage(),
+          'flip_image': (_) => _flipImagePage(),
+          'flip_stream': (_) => _flipStreamPage(),
+          'flip_frase_board': (_) => FlipFraseBoardPage(),
+          'flip_clock': (_) => FlipClockPage(),
+          'countdown_clock': (_) => CountdownClockPage(),
+        },
+        home: HomePage(),
+      );
 
-  Widget _flipWidgetPage(BuildContext context) => FlipWidgetPage();
-
-  Widget _flipImagePage(BuildContext context) =>
-      const FlipMatrixBoardSingleChildPage(
+  Widget _flipImagePage() => const FlipMatrixBoardSingleChildPage(
         imageName: 'assets/flutter.png',
         axis: Axis.vertical,
         width: 375.0,
@@ -39,81 +34,97 @@ class MyApp extends StatelessWidget {
         rows: 4,
       );
 
-  Widget _flipStreamPage(BuildContext context) {
-    final images = <String>[
-      'assets/flower.png',
-      'assets/butterfly.png',
-      'assets/sea.png',
-      'assets/bird.png',
-    ];
-    return FlipMatrixBoardStreamPage(
-      imageNames: images,
-      axis: Axis.vertical,
-      width: 375.0,
-      height: 200.0,
-      columns: 8,
-      rows: 4,
-      animationMillis: 4000,
-      imageChangeSeconds: 6,
-    );
-  }
+  Widget _flipStreamPage() => FlipMatrixBoardStreamPage(
+        axis: Axis.vertical,
+        width: 375.0,
+        height: 200.0,
+        columns: 8,
+        rows: 4,
+        animationMillis: 4000,
+        imageChangeSeconds: 6,
+        imageNames: [
+          'assets/flower.png',
+          'assets/butterfly.png',
+          'assets/sea.png',
+          'assets/bird.png',
+        ],
+      );
 }
 
 class HomePage extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Flip Board')),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ListTileTheme(
-          tileColor: Colors.black12,
-          contentPadding: const EdgeInsets.all(8.0),
-          shape: Border.all(color: Colors.black38),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              ListTile(
-                leading: const Icon(Icons.flip, size: 48.0),
-                title: const Text('Flip Widget'),
-                subtitle: const Text('Simple Flip Widgets'),
-                onTap: () => Navigator.of(context).pushNamed('flip_widget'),
-              ),
-              ListTile(
-                leading: const Icon(Icons.dashboard, size: 48.0),
-                title: const Text('Flip Image Board'),
-                subtitle: const Text('Animate the display of a single image'),
-                onTap: () => Navigator.of(context).pushNamed('flip_image'),
-              ),
-              ListTile(
-                leading: const Icon(Icons.blur_on, size: 48.0),
-                title: const Text('Flip Stream Board'),
-                subtitle: const Text('Animate a stream of images'),
-                onTap: () => Navigator.of(context).pushNamed('flip_stream'),
-              ),
-              ListTile(
-                leading: const Icon(Icons.sort_by_alpha, size: 48.0),
-                title: const Text('Flip Frase Board'),
-                subtitle: const Text('Flip FLUTTER FLIP BOARD'),
-                onTap: () =>
-                    Navigator.of(context).pushNamed('flip_frase_board'),
-              ),
-              ListTile(
-                leading: const Icon(Icons.watch, size: 48.0),
-                title: const Text('Flip Clock'),
-                subtitle: const Text('A nice looking clock'),
-                onTap: () => Navigator.of(context).pushNamed('flip_clock'),
-              ),
-              ListTile(
-                leading: const Icon(Icons.run_circle, size: 48.0),
-                title: const Text('Flip Countdown Clock'),
-                subtitle: const Text('A nice looking countdown clock'),
-                onTap: () => Navigator.of(context).pushNamed('countdown_clock'),
-              ),
-            ],
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(title: const Text('Flip Board')),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListTileTheme(
+            tileColor: Colors.black12,
+            contentPadding: const EdgeInsets.all(8.0),
+            shape: Border.all(color: Colors.black38),
+            child: ListView(
+              children: [
+                _tile(
+                  context,
+                  Icons.flip,
+                  'Flip Widget',
+                  'Simple Flip Widgets',
+                  'flip_widget',
+                ),
+                _tile(
+                  context,
+                  Icons.dashboard,
+                  'Flip Image Board',
+                  'Animate the display of a single image',
+                  'flip_image',
+                ),
+                _tile(
+                  context,
+                  Icons.blur_on,
+                  'Flip Stream Board',
+                  'Animate a stream of images',
+                  'flip_stream',
+                ),
+                _tile(
+                  context,
+                  Icons.sort_by_alpha,
+                  'Flip Frase Board',
+                  'Flip FLUTTER FLIP BOARD',
+                  'flip_frase_board',
+                ),
+                _tile(
+                  context,
+                  Icons.watch,
+                  'Flip Clock',
+                  'A nice looking clock',
+                  'flip_clock',
+                ),
+                _tile(
+                  context,
+                  Icons.run_circle,
+                  'Flip Countdown Clock',
+                  'A nice looking countdown clock',
+                  'countdown_clock',
+                ),
+              ],
+            ),
           ),
         ),
-      ),
-    );
-  }
+      );
+
+  Widget _tile(
+    BuildContext context,
+    IconData icon,
+    String title,
+    String subtitle,
+    String route,
+  ) =>
+      Padding(
+        padding: const EdgeInsets.symmetric(vertical: 4.0),
+        child: ListTile(
+          leading: Icon(icon, size: 48.0),
+          title: Text(title),
+          subtitle: Text(subtitle),
+          onTap: () => Navigator.of(context).pushNamed(route),
+        ),
+      );
 }
